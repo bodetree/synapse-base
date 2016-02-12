f757dd876fa32cb18c1981226c42fd458d2c9827
Ben Youngblood <bx.youngblood@gmail.com>
2016-02-11 23:56:33 -0700

    Revert "worker command ignores --count flag (always 1)"
    This reverts commit ee185f325d3c68e8698021151f5a8878addec771.

--- src/Synapse/Resque/ResqueCommand.php
+++ src/Synapse/Resque/ResqueCommand.php
@@ -107,9 +107,66 @@ class ResqueCommand implements CommandInterface, LoggerAwareInterface
      */
     protected function startWorkers($queues, $count = 1, $interval = 5)
     {
-        $worker = new Resque_Worker($queues);
-        $worker->setLogger($this->logger);
-        $worker->hasParent = FALSE;
-        $worker->work($interval);
+        $children = array();
+        $GLOBALS['send_signal'] = FALSE;
+
+        $dieSignals = array(SIGTERM, SIGINT, SIGQUIT);
+        $allSignals = array_merge($dieSignals, array(SIGUSR1, SIGUSR2, SIGCONT, SIGPIPE));
+
+        for ($i = 0; $i < $count; ++$i) {
+            $pid = pcntl_fork();
+
+            if ($pid === -1) {
+                // Could not fork
+                $this->output->writeln('<error>Could not fork worker '.$i.'</error>');
+                return;
+            } elseif (! $pid) {
+                // Child now
+                $worker = new Resque_Worker($queues);
+                $worker->setLogger($this->logger);
+                $worker->hasParent = TRUE;
+
+                $this->output->writeln('<info>*** Starting worker '.$worker.'</info>');
+                $worker->work($interval);
+
+                // Have to break now to stop the child from forking! This will
+                // not run in the parent.
+                break;
+            } else {
+                $children[$pid] = 1;
+                while (count($children) == $count) {
+                    if (! isset($registered)) {
+                        declare(ticks = 1);
+                        foreach ($allSignals as $signal) {
+                            pcntl_signal($signal, array('Synapse\\Resque\\ResqueCommand', 'cleanup_children'));
+                        }
+
+                        $registered = TRUE;
+                    }
+
+                    $childPid = pcntl_waitpid(-1, $childStatus, WNOHANG);
+                    if ($childPid != 0) {
+                        $this->output->writeln('<error>A child worker died: '.$childPid.'</error>');
+                        unset($children[$childPid]);
+                        $i--;
+                    }
+
+                    usleep(250000);
+
+                    if ($GLOBALS['send_signal'] !== FALSE) {
+                        foreach ($children as $k => $v) {
+                            posix_kill($k, $GLOBALS['send_signal']);
+                            if (in_array($GLOBALS['send_signal'], $dieSignals)) {
+                                pcntl_waitpid($k, $childStatus);
+                            }
+                        }
+                        if (in_array($GLOBALS['send_signal'], $dieSignals)) {
+                            exit;
+                        }
+                        $GLOBALS['send_signal'] = FALSE;
+                    }
+                }
+            }
+        }
     }
 }
